d05dcf54828f0e813b9c405b562a6401d87afa0c
Davii1 <david2002rg@hotmail.com>
2024-01-18 11:58:27 -0600

    changes nuxt.config removing github provider credentials

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,10 +16,6 @@ export default defineNuxtConfig({
     authJs: {
       secret: process.env.NUXT_NEXTAUTH_SECRET, // You can generate one with `openssl rand -base64 32`
     },
-    github: {
-      clientId: process.env.NUXT_GITHUB_CLIENT_ID,
-      clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET,
-    },
     public: {
       authJs: {
         baseUrl: process.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
